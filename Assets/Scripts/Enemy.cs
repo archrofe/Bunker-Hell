362e30417178701df33db2675a9d72b338690a0e
Duane Reymond <duane.reymond@gmail.com>
2017-10-23 09:38:51 +1100

    -Enemy and Tower scripts added. Only contains variables (Health for Enemy, Attack Rate and Attack Radius for Tower) for use in scripting the Upgrade system, whilst both scripts are being completed.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 100f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Enemy health = " + health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public float attackRate = 0.25f;
+    public float attackRadius = 5f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Tower attackRate = " + attackRate);
+        Debug.Log("Tower attackRadius = " + attackRadius);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
