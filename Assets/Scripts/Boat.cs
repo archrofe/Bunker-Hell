96e9b22412363ba46bd318fef5f49f7e88d99d86
Duane Reymond <duane.reymond@gmail.com>
2017-10-22 12:41:10 +1100

    -EnemySpawner Script and GameObject working for all 6 Boats.

--- Assets/Scripts/Boat.cs
+++ Assets/Scripts/Boat.cs
@@ -10,13 +10,6 @@ public class Boat : MonoBehaviour
     public float boatSpeed;
     public bool boatOnBeach;
 
-    [Header("Spawner")]
-    public GameObject[] spawnPrefabs;
-    public float spawnRadius = 1f;
-    public int spawnAmount = 1;
-    public float spawnInterval = 1f;
-    public bool hasSpawned = false;
-
     // Use this for initialization
     void Start()
     {
@@ -27,7 +20,6 @@ public class Boat : MonoBehaviour
     void Update()
     {
         LeftAndRightMovement();
-        SpawnLocation();
     }
 
     #region Movement
@@ -62,60 +54,6 @@ public class Boat : MonoBehaviour
     void GoLeft()
     {
         transform.position = Vector3.MoveTowards(transform.position, boatPos1.position, boatSpeed * Time.deltaTime);
-    }
-    #endregion
-
-    #region Spawner
-    void SpawnEnemy()
-    {
-        for (int i = 0; i < spawnAmount; i++)
-        {
-            // Spawned new GameObject
-            int randomIndex = Random.Range(0, spawnPrefabs.Length);
-
-            // Store randomly selected prefab
-            GameObject randomPrefab = spawnPrefabs[randomIndex];
-
-            // Spawned new GameObject
-            GameObject clone = Instantiate(randomPrefab);
-
-            // Calculate random position within sphere
-            Vector3 randomPos = transform.position + Random.insideUnitSphere * spawnRadius; // calculate random position
-
-            // Cancel out the Z
-            randomPos.z = 0;
-
-            // Set spawned object's position
-            clone.transform.position = randomPos;
-        }
-    }
-
-    void OnDrawGizmos()
-    {
-        Gizmos.DrawWireSphere(transform.position, spawnRadius);
-    }
-
-    void SpawnLocation()
-    {
-        if (transform.position == boatPos2.position)
-        {
-            Debug.Log("SpawnLocation reached");
-            SpawnNow();
-        }
-    }
-
-    IEnumerator SpawnNow()
-    {
-        // run whatever is here first
-        hasSpawned = true;
-
-        // Spawn the bullet
-        SpawnEnemy();
-
-        yield return new WaitForSeconds(spawnInterval); // wait a few seconds
-
-        // run whatever is here last
-        hasSpawned = false;
     }
     #endregion
 }
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject[] spawnPrefabs;
+    public float spawnRadius = 1f;
+    public int spawnAmount = 1;
+    public int currentSpawn;
+    public int maxSpawn = 1;
+    public int spawnInterval = 1;
+    private bool hasSpawned = false;
+
+    void Start()
+    {
+        currentSpawn = 0;
+    }
+
+    void Update()
+    {        
+
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, spawnRadius);
+    }
+
+    void SpawnBalls()
+    {
+        for (int i = 0; i < spawnAmount; i++)
+        {
+            // Spawned new GameObject
+            int randomIndex = Random.Range(0, spawnPrefabs.Length);
+
+            // Store randomly selected prefab
+            GameObject randomPrefab = spawnPrefabs[randomIndex];
+
+            // Spawned new GameObject
+            GameObject clone = Instantiate(randomPrefab);
+
+            // Calculate random position within sphere
+            Vector3 randomPos = transform.position + Random.insideUnitSphere * spawnRadius; // calculate random position
+
+            // Lock the Y
+            randomPos.y = 1;
+
+            // Set spawned object's position
+            clone.transform.position = randomPos;
+        }
+    }
+
+    IEnumerator SpawnNow()
+    {
+        // run whatever is here first
+        hasSpawned = true;
+
+        // Spawn the Enemy
+        SpawnBalls();
+
+        yield return new WaitForSeconds(spawnInterval); // wait a few seconds
+
+        // run whatever is here last
+        hasSpawned = false;
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.CompareTag("Boat"))
+        {
+            if (!hasSpawned && currentSpawn < maxSpawn)
+            {
+                StartCoroutine(SpawnNow());
+                currentSpawn++;
+            }
+        }
+    }
+}
