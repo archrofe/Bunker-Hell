a2e5c57cdfaddb342ea82e714ff16e79c83716fd
Jayderade <jcastleden@gmail.com>
2017-10-31 14:08:17 +1100

    TIME AND GOLD

---
+++ Assets/Scripts/Economy/EconomyScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EconomyScript : MonoBehaviour
+{
+
+    public float timer;
+    public Text theTime;
+    public Text gold;
+    public float moneys;
+    public int totalGold;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = 0f;
+        moneys = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        totalGold = Mathf.RoundToInt(moneys);
+
+        moneys = moneys + 5 * Time.deltaTime;
+
+        timer = timer + 1 * Time.deltaTime;
+
+        SetTimer();
+        SetGold();
+    }
+
+    void SetGold()
+    {
+        gold.text = "Gold:" + totalGold.ToString();
+    }
+
+    void SetTimer()
+    {
+        theTime.text = "Timer:" + timer.ToString("0.0");
+    }
+}
