5f17719107d50d2915e2cdee13c8235be60c36d3
Jayderade <jcastleden@gmail.com>
2017-11-02 21:15:50 +1100

    FIXED THE MUTHAFUKING ECONOMY

--- Assets/Scripts/Economy/EconomyScript.cs
+++ Assets/Scripts/Economy/EconomyScript.cs
@@ -11,23 +11,28 @@ public class EconomyScript : MonoBehaviour
     public Text gold;
     public float moneys;
     public int totalGold;
-
+    public static float enemyHealth;
 
     // Use this for initialization
     void Start()
     {
         timer = 0f;
         moneys = 0;
+
+        enemyHealth = 100;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        EarnedMoney();
+
+        enemyHealth = Enemy.health;
+
         totalGold = Mathf.RoundToInt(moneys);
 
-        moneys = moneys + 5 * Time.deltaTime;
+        moneys = moneys + 1 * Time.deltaTime;
 
         timer = timer + 1 * Time.deltaTime;
 
@@ -35,6 +40,20 @@ public class EconomyScript : MonoBehaviour
         SetGold();
     }
 
+    void EarnedMoney()
+    {
+        if (enemyHealth == 0)
+        {
+
+            KillGold();
+            Enemy.health = 100;
+        }
+    }
+
+    void KillGold()
+    {
+        moneys = moneys + 100;
+    }
     void SetGold()
     {
         gold.text = "Gold:" + totalGold.ToString();
--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,20 +7,18 @@ using UnityEngine.UI;
 public class Enemy : MonoBehaviour
 {
     
-    public float health = 100f; // Enemy's health which starts at 100
-    public int money;
-    
+    public static float health = 100f; // Enemy's health which starts at 100
+          
 
     public Text gold;
 
     public void DealDamage(float damage)
     {
-        health -= damage;
+        health -= damage;       
         
+
         if (health <= 0)
-        {            
-            
-            Gold();
+        {           
             Destroy(gameObject);
         }
     }
@@ -35,11 +33,7 @@ public class Enemy : MonoBehaviour
         
     }
 
-    void Gold()
-    {
-        money = money + 100;
-        gold.text = money.ToString();
-    }
+   
 
     // Update is called once per frame
     void Update()
