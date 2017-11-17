b5eb351ee0ea9bc93586f5439db3818d8817b35f
Jayderade <jcastleden@gmail.com>
2017-11-17 14:00:38 +1100

    Made it exit to main menu after u lose

--- Assets/Scripts/Lose.cs
+++ Assets/Scripts/Lose.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Lose : MonoBehaviour
 {
@@ -11,10 +12,12 @@ public class Lose : MonoBehaviour
     public static float time;
     public Text score;
     public Image bGround;
+    public bool lost;
 
     // Use this for initialization
     void Start()
     {
+        lost = false;
         bGround.enabled = false;
         score.enabled = false;
         lose.enabled = false;
@@ -26,15 +29,29 @@ public class Lose : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         time = EconomyScript.timer;
         if (loseScore == 0)
         {
             //Debug.Log("works");
-            Time.timeScale = 0;
+            StartCoroutine(BackToMain());
             score.text = "Your Time: " + time.ToString("0.0");
             lose.enabled = true;
             score.enabled = true;
             bGround.enabled = true;
+            loseScore = 1;
+            
         }
+    }
+
+    IEnumerator BackToMain()
+    {
+        Debug.Log("Before");
+
+        yield return new WaitForSeconds(2);
+
+        Debug.Log("After");
+
+        SceneManager.LoadScene("Menu");
     }
 }
